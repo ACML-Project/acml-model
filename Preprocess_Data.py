6dc85194e433b0c0401fa01ba827f5bc8f21f7fa
NashTheHash <2554397@students.wits.ac.za>
2025-05-12 19:51:05 +0200

    Created merged csv to use for training

---
+++ Preprocess_Data.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import csv
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
+
+def Get_Dataset():
+
+    fake_news = pd.read_csv('Fake.csv')
+    true_news = pd.read_csv('True.csv')
+
+    #ADD LABELS
+    fake_news['label'] = 0
+    true_news['label'] = 1
+    dataset = pd.concat([fake_news, true_news], axis=0)
+
+    #RETURN SHUFFLED DATASET
+    return dataset.sample(frac=1).reset_index(drop=True)
+
+
+def Preprocess_Text(text):
+
+    text = text.lower()
+
+    #REMOVE SPECIAL CHARACTERS
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+
+    #TOKENIZE
+    words = text.split()
+
+    #REMOVE STOPWORDS AND LEMMATIZE
+    words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
+    return ' '.join(words)
+
+
+dataset = Get_Dataset()
+dataset['text'] = dataset['text'].apply(Preprocess_Text)
+
+#SAVING OUR DATASET AS A NEW CSV SO WE DON'T HAVE TO PREPROCESS EVERY TIME
+dataset.to_csv("Merged.csv", index=False)
