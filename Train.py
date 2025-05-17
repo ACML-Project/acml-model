d09eb19c48345026811b211cc394c6b5bcda8bff
NashTheHash <2554397@students.wits.ac.za>
2025-05-17 22:38:46 +0200

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,14 +1,26 @@
+
+
 learning_rate = 1
 num_epochs = 20
 batch_size = 32
 max_len = 128
 
 
-dataset = 0
-
 #INDEPENDENT 
-X = dataset.drop('label', axis=1)
+#X = dataset.drop('label', axis=1)
 
 #DEPENDENT
-Y = dataset['label']
+#Y = dataset['label']
 
+#print(len(vocab))
+#output_file = open("output.txt", "w")
+#print(vocab, file=output_file)
+#https://www.youtube.com/watch?v=k3_qIfRogyY
+#return ' '.join(processed_words)
+
+
+#KEEP TRACK OF THE LONGEST ARTICLE
+    #global MAX_ARTICLE_LEN
+    #len_curr = len(processed_text)
+    #if len_curr > MAX_ARTICLE_LEN:
+    #    MAX_ARTICLE_LEN = len_curr
