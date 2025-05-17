7981b930cee98ff6b8db4b05bf948cb94e92c272
NashTheHash <2554397@students.wits.ac.za>
2025-05-17 22:37:46 +0200

    Added embedding

--- Preprocess_Text.py
+++ Preprocess_Text.py
@@ -1,46 +1,60 @@
-import nltk
 import pandas as pd
-import re
-from collections import Counter
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
+import re
+import torch.nn as nn
 
 
 nltk.download('stopwords')
 nltk.download('wordnet')
-nltk.download('punkt_tab')
-
+nltk.download('punkt')
 
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 special_tokens = ['<pad>', '<sos>', '<eos>', '<unk>']
 
+MAX_ARTICLE_LEN = 512
+EMBEDDING_DIM = 128
+PAD_INDEX = 0
+SOS_INDEX = 1
+EOS_INDEX = 2
+UNK_INDEX = 3
+
 
 def Preprocess_Text(text):
 
-    text = text.lower()
+    processed_text = []
 
-    #REMOVE SPECIAL CHARACTERS
-    text = re.sub(r'[^a-zA-Z\s]', ' ', text)
+    #TOKENIZE: SPLIT INTO SENTENCES
+    sentences = sent_tokenize(text)
 
-    #DEALS WITH WHITESPACE
-    text = re.sub(r'\s+', ' ', text).strip()
+    for sentence in sentences:
 
-    #TOKENIZE
-    words = word_tokenize(text)
+        #REMOVE SPECIAL CHARACTERS, DEALS WITH WHITESPACE AND MAKES SENTENCE LOWERCASE
+        processed_sentence = re.sub(r'[^a-zA-Z\s]', ' ', sentence.lower()).strip()
 
-    #REMOVE STOPWORDS AND LEMMATIZE
-    processed_words = []
+        #TOKENIZE: SPLIT INTO WORDS
+        words = word_tokenize(processed_sentence)
 
-    for word in words:
-        if word not in stop_words and word not in processed_words:
-            processed_words.append(lemmatizer.lemmatize(word))
+        #REMOVE STOPWORDS AND LEMMATIZE
+        processed_words = []
 
-    #ADD START- AND END-OF-SENTENCE MARKERS
-    processed_words = [ special_tokens[1] ] + processed_words + [ special_tokens[2] ]
+        for word in words:
+            if word not in stop_words:
+               processed_words.append(lemmatizer.lemmatize(word))
 
-    return processed_words
+        #ADD START- AND END-OF-SENTENCE MARKERS
+        processed_words = [ special_tokens[SOS_INDEX] ] + processed_words + [ special_tokens[EOS_INDEX] ]
+        
+        for word in processed_words:
+            processed_text.append(word)
+ 
+    #TRUNCATES TEXT TO MAX LENGTH
+    del processed_text[MAX_ARTICLE_LEN:]
+    
+    return processed_text
 
 
 def Build_Vocab(tokenized_text):
@@ -56,15 +70,33 @@ def Build_Vocab(tokenized_text):
             else: vocab[token] = vocab.get(token) + 1
 
     #REMOVE WORDS THAT DON'T APPEAR OFTEN
-    return [ token for token, i in vocab.items() if token in special_tokens or i > 2 ]
+    vocab = [ token for token, i in vocab.items() if token in special_tokens or i > 2 ]
+
+    #RETURN AN ENUMERATED VOCAB
+    return { vocab[i] : i for i in range( len(vocab) ) }
 
 
-training_data = pd.read_csv('Training.csv')
-processed_text = training_data['text'].apply(Preprocess_Text)
-vocab = Build_Vocab(processed_text)
+def Add_Padding(processed_data):
 
-#print(len(vocab))
-#output_file = open("output.txt", "w")
-#print(vocab, file=output_file)
-#https://www.youtube.com/watch?v=k3_qIfRogyY
-#return ' '.join(processed_words)
+    for article in processed_data:
+        n = MAX_ARTICLE_LEN - len(article)
+
+        for i in range(n):
+            article.append(special_tokens[PAD_INDEX])
+
+    return processed_data
+
+
+dataset = pd.read_csv('Training.csv')
+processed_data = dataset['text'].apply(Preprocess_Text)
+vocab = Build_Vocab(processed_data)
+processed_data = Add_Padding(processed_data)
+
+embedding = nn.Embedding(
+    num_embeddings = len(vocab),
+    embedding_dim = EMBEDDING_DIM,
+    padding_idx = PAD_INDEX
+)
+
+#output_file = open("processed_data.txt", "w")
+#print(*processed_data, file=output_file)
